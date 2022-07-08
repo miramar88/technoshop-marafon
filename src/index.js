10e1190fc7d2df02182eecffb3a58f85a1487d24
Margarita Smirnova <bobcat0606@gmail.com>
2022-07-08 09:02:07 +0300

    Стилизация карточки товара, подключен слайдер

--- src/index.js
+++ src/index.js
@@ -1,4 +1,26 @@
-import './index.html';
-import './cart.html';
-import './card.html';
-import './index.scss';
+import "./index.html";
+import "./cart.html";
+import "./card.html";
+import "./index.scss";
+
+import Swiper, {Thumbs} from 'swiper';
+import 'swiper/css';
+
+const thumbSwiper = new Swiper(".card__preview", {
+  spaceBetween: 44,
+  slidesPerView: 5
+}); 
+
+new Swiper(".card__photo", {
+  spaceBetween: 10,
+  slidesPerView: 1,
+  thumbs: {
+    swiper: thumbSwiper,
+  },
+  modules: [Thumbs],
+});
+
+new Swiper(".recommend__carousel", {
+  spaceBetween: 30,
+  slidesPerView: 5
+});
